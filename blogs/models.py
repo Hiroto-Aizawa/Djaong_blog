327ac25f3f08e8c042588f2a0e8ef73baf73ea8d
Hiroto-Aizawa <r_20love@icloud.com>
2025-07-27 23:21:28 +0900

    ブログモデルの作成

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#インポートしたmodelsを使って、それを継承したBlogクラスを作成する
+# モデルを作成する際は「Blogs」のように複数形ではなく「Blog」のように単数形で作成する
+# これによって「Blogs」という名称のテーブルを自動生成してくれる
+class Blog(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=150)
+    text = models.TextField()
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
